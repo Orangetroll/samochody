c169523049d191c2e8ddc449d563c50c9bd154cd
Marek Piotrowicz <maaro2001@gmail.com>
2023-11-03 01:36:51 +0100

    lab2

--- samochody_web/models.py
+++ samochody_web/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title =  models.CharField(max_length=150, blank=False, unique=False)
-    year = models.PositiveSmallIntegerField(default=2023)
+    NAPEDY = [
+        ('przedninaped', 'Przedni napęd'),
+        ('tylnaped', 'Tylni naped'),
+        ('4x4', "Napęd 4x4"),
+    ]
+
+    marka = models.CharField(max_length=30, blank=False, unique=False)
+    model = models.CharField(max_length=30, blank=False, unique=False)
+    silnik = models.CharField(max_length=10, blank=False, unique=False)
+    skrzynia_biegów = models.CharField(max_length=20, blank=False, unique=False)
+    drzwi = models.IntegerField(blank=False, unique=False)
+    rok = models.PositiveSmallIntegerField(default=2023)
+    naped = models.CharField(max_length=20, choices=NAPEDY, blank=False, unique=False)
 
     def __str__(self):
-        return self.title
+        return self.marka + self.model + self.silnik + self.skrzynia_biegów + self.drzwi + self.rok + self.naped
